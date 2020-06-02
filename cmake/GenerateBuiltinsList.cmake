faf07380240f8e8df036e93ce76396964bae4027
Sleepy Flower Girl <wuhao64@gmail.com>
2020-06-02 11:49:43 -0400

    Fix bytecode gen not running after gen is build

--- cmake/GenerateBuiltinsList.cmake
+++ cmake/GenerateBuiltinsList.cmake
@@ -1,15 +1,17 @@
-function(v8_generate_builtins_list target-dir)
+function(v8_generate_builtins_list target-dir target)
   set(directory ${target-dir}/builtins-generated)
   set(output ${directory}/bytecodes-builtins-list.h)
   add_custom_command(
+    PRE_BUILD
     COMMAND ${CMAKE_COMMAND} -E make_directory ${directory}
     OUTPUT ${directory}
     COMMENT "Generating ${directory}"
     VERBATIM)
   add_custom_command(
+    TARGET ${target} 
+		POST_BUILD
     COMMAND bytecode_builtins_list_generator ${output}
     DEPENDS ${directory}
-    OUTPUT ${output}
     COMMENT "Generating ${output}"
     VERBATIM)
   add_library(v8-bytecodes-builtin-list INTERFACE)
